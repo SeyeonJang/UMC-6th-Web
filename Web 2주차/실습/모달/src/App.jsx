f227cb7e3e2c4d23f4356e746990619d406a628d
SeyeonJang <seye0n.bliss@gmail.com>
2024-04-08 11:52:42 +0900

    feat: [#6] 모달 - 모달 열기 버튼이 있는 UI 추가

---
+++ Web 2주차/실습/모달/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>안녕하세요!</h1>
+      <h3>챠코입니다~</h3>
+      <button id="btn_open_modal">버튼 열기</button>
+    </>
+  )
+}
+
+export default App
