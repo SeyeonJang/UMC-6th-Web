f80cb5eb8bd535ef4d18ba6d246941e564372c24
SeyeonJang <seye0n.bliss@gmail.com>
2024-04-08 12:39:15 +0900

    feat: [#6] 기본 CSS 제거, 모달 CSS 추가

--- Web 2주차/실습/모달/components/Modal.jsx
+++ Web 2주차/실습/모달/components/Modal.jsx
@@ -1,15 +1,21 @@
+import '../styles/Modal.css'
+
 function Modal() {
     return (
-        <div class="modal">
-            <div class="modalContent">
-                <div class="modalText">
-                    <h3>안녕하세요</h3>
-                    <p>모달 내용은 어쩌고 저쩌고..</p>
-                </div>
-                <div class="modalButton">
-                    <button id="btn_close_modal">닫기</button>
+        <>
+            <div class="modal">
+                <div class="modalContent">
+                    <div class="modalText">
+                        <h2>안녕하세요</h2>
+                        <p><b>모달 내용은 어쩌고 저쩌고..</b></p>
+                    </div>
+                    <div class="modalButton">
+                        <button id="btn_close_modal">닫기</button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
+
+export default Modal;
--- Web 2주차/실습/모달/src/App.jsx
+++ Web 2주차/실습/모달/src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Modal from '../components/Modal.jsx'
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+
   return (
     <>
       <h1>안녕하세요!</h1>
-      <h3>챠코입니다~</h3>
+      <p>내용내용내용</p>
       <button id="btn_open_modal">버튼 열기</button>
-
-      
+      <Modal/>
     </>
   )
 }
