4f37d548ba231023887de0044f5b2a357126db1c
SeyeonJang <seye0n.bliss@gmail.com>
2024-04-09 01:09:07 +0900

    feat: [#7] input 창에 입력 시 해야할 일 컴포넌트가 생기는 로직 추가

--- Web 2주차/미션1 - Todo/components/TodoComponent.jsx
+++ Web 2주차/미션1 - Todo/components/TodoComponent.jsx
@@ -1,16 +1,14 @@
 import '../styles/Todo.css'
 
-function TodoComponent() {
+function TodoComponent({text}) {
     return (
-        <>
-            <div class="oneTodo">
-                <div class="spanTodo">
-                    <h4 id="text_todo_left">HTML</h4>
-                    <button id="todo_complete">완료</button>
-                </div>
-                <hr class="hr_middle"/>
+        <div class="oneTodo">
+            <div class="spanTodo">
+                <h4 id="text_todo_left">{text}</h4> {/* props로 받은 text */}
+                <button id="todo_complete">완료</button>
             </div>
-        </>
+            <hr class="hr_middle"/>
+        </div>
     )
 }
 
--- Web 2주차/미션1 - Todo/src/App.jsx
+++ Web 2주차/미션1 - Todo/src/App.jsx
@@ -4,18 +4,32 @@ import TodoComponent from '../components/TodoComponent.jsx'
 import CompletedComponent from '../components/CompletedComponent.jsx'
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleEnterKey = (event) => {
+    if (event.key === 'Enter') {
+      const newTodo = event.target.value;
+      setTodos([...todos, newTodo]); // 기존에 todos 배열과 newTodo 배열을 합쳐서 만들기
+      setInputValue(""); // 입력창 초기화
+    }
+  }
+
   return (
     <>
       <div className='bodyBox'>
         <h1>UMC Study Plan</h1>
         <hr id="hr_big"/>
-        <input type="text" id="todo_input" onkeyup="enterkey()" placeholder='UMC 스터디 계획을 작성해보세요!'></input>
+        <input type="text" id="todo_input" value={inputValue} placeholder='UMC 스터디 계획을 작성해보세요!'
+          onChange={(e) => setInputValue(e.target.value)} onKeyDown={handleEnterKey}/>
 
         <div class="todoText">
           <div class="todoBox">
               <h4>해야 할 일</h4>
               <hr id="hr_small"/>
-              <TodoComponent/>
+              {todos.map((todo,index) => (
+                <TodoComponent key={index} text={todo}/>
+              ))}
               
           </div>
           <div class="todoBox">
