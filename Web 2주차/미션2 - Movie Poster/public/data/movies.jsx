b7b0b2b82f8b3981cf242781029a3dd662dcb364
SeyeonJang <seye0n.bliss@gmail.com>
2024-04-09 12:38:19 +0900

    feat: [#7] MoviePoster - mockup API 파일 추가 및 연결

---
+++ Web 2주차/미션2 - Movie Poster/public/data/movies.jsx
@@ -0,0 +1,73 @@
+// movies.js
+
+export const movies = {
+    "dates": {
+      "maximum": "2023-09-17",
+      "minimum": "2023-07-31"
+    },
+    "page": 1,
+    "results": [
+      {
+        "adult": false,
+        "backdrop_path": "/8pjWz2lt29KyVGoq1mXYu6Br7dE.jpg",
+        "genre_ids": [
+          28,
+          878,
+          27
+        ],
+        "id": 615656,
+        "original_language": "en",
+        "original_title": "Meg 2: The Trench",
+        "overview": "전 세계에서 가장 깊은 마리아나 해구, 역사상 가장 거대하고 무자비한 포식자가 도사리는 그곳에서 강철 다이버 ‘조나스’(제이슨 스타뎀)가 더 맹렬하게 돌아온 ‘메가로돈’과 짜릿한 대결을 펼치는 액션 어드벤처",
+        "popularity": 2429.447,
+        "poster_path": "/cbAHK6Vrt0GClMRUxH8TsgC2JqL.jpg",
+        "release_date": "2023-08-15",
+        "title": "메가로돈 2",
+        "video": false,
+        "vote_average": 7,
+        "vote_count": 1790
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/w2nFc2Rsm93PDkvjY4LTn17ePO0.jpg",
+        "genre_ids": [
+          16,
+          35,
+          28
+        ],
+        "id": 614930,
+        "original_language": "en",
+        "original_title": "Teenage Mutant Ninja Turtles: Mutant Mayhem",
+        "overview": "뉴욕의 하수구로 스며든 미스터리한 녹색 액체, ‘우즈’로 인해 아기 거북이 네 마리와 청년 쥐 스플린터는 사람과 비슷한 모습의 돌연변이가 된다. 15년 후, 10대 청소년이 된 레오나르도, 미켈란젤로, 라파엘, 도나텔로는 하수구에 숨어 사는 생활에서 벗어나 인간 세상에서 평범한 삶을 살고 싶어하고 우연히 만난 학생 기자 에이프릴과 함께 도시를 위협하는 '슈퍼플라이'와 그 일당을 물리치면 뉴욕의 히어로가 되어 당당히 세상에 나갈 수 있을 거라는 꿈을 꾸는데… 인간들을 모두 돌연변이로 만들겠다는 ‘슈퍼플라이’의 음모로 위험에 빠진 뉴욕! 틴에이저 히어로로 다시 태어난 ‘닌자터틀’ 형제들의 스트릿 액션 어드벤처가 펼쳐진다!",
+        "popularity": 800.947,
+        "poster_path": "/6xORIhe6cP3iM5RYTHs7MTOH1qa.jpg",
+        "release_date": "2023-09-14",
+        "title": "닌자터틀: 뮤턴트 대소동",
+        "video": false,
+        "vote_average": 7.3,
+        "vote_count": 512
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/fm6KqXpk3M2HVveHwCrBSSBaO0V.jpg",
+        "genre_ids": [
+          18,
+          36
+        ],
+        "id": 872585,
+        "original_language": "en",
+        "original_title": "Oppenheimer",
+        "overview": "세상을 구하기 위해 세상을 파괴할 지도 모르는 선택을 해야 하는 천재 과학자의 핵개발 프로젝트.",
+        "popularity": 530.049,
+        "poster_path": "/4ZLnVUfiCe3wX8Ut9eyujndpyvA.jpg",
+        "release_date": "2023-08-15",
+        "title": "오펜하이머",
+        "video": false,
+        "vote_average": 8.3,
+        "vote_count": 3566
+      }
+    ],
+    "total_pages": 5,
+    "total_results": 83
+  };
+  
---
+++ Web 2주차/미션2 - Movie Poster/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { movies } from '../public/data/movies.jsx';
+
+function App() {
+  console.log(movies);
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
