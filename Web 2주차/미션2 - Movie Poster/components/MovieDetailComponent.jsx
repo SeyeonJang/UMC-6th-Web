289e39df9bb1435d44d5557a0dd1421a1cae23f9
SeyeonJang <seye0n.bliss@gmail.com>
2024-04-12 23:59:36 +0900

    feat: [#7] MoviePoster - 영화 상세 설명 컴포넌트 UI & CSS 추가 및 데이터와 연결

--- Web 2주차/미션2 - Movie Poster/components/MovieDetailComponent.jsx
+++ Web 2주차/미션2 - Movie Poster/components/MovieDetailComponent.jsx
@@ -4,10 +4,8 @@ function MovieDetailComponent({title, overview}) {
     return (
         <div className='movieDetailBox'>
             <div className='textDetailBox'>
-                {/* <p id='title_detail'>{title}</p> */}
-                <p id='title_detail'>타이틀</p>
-                {/* <p id='overview'>{overview}</p> */}
-                <p id='overview'>오버뷰</p>
+                <p id='title_detail'>{title}</p>
+                <p id='overview'>{overview}</p>
             </div>
         </div>
     )
--- Web 2주차/미션2 - Movie Poster/src/App.jsx
+++ Web 2주차/미션2 - Movie Poster/src/App.jsx
@@ -5,35 +5,83 @@ import MovieDetailComponent from '../components/MovieDetailComponent.jsx';
 import './App.css';
 
 function App() {
-  // const newImagePath = originalImagePath.replace("/", "https://image.tmdb.org/t/p/w500/");
-  const [MovieComponents, setMovieComponents] = useState([]);
+  // 실패한 코드 ,,
+  // const [MovieComponents, setMovieComponents] = useState([]);
+  // const [MovieDetailComponents, setMovieDetailComponents] = useState([]);
 
-  const generateMovieComponents = () => {
-    const newMovieComponents = movies.results.map(movie => (
-      <MovieComponent
-        key={movie.id}
-        image={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-        title={movie.title}
-        voteAverage={movie.vote_average}
-      />
-    ));  
-    setMovieComponents(newMovieComponents);
-  }
+  // const generateMovieComponents = () => {
+  //   const newMovieComponents = movies.results.map(movie => (
+  //     <MovieComponent
+  //       key={movie.id}
+  //       image={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+  //       title={movie.title}
+  //       voteAverage={movie.vote_average}
+  //     />
+  //   ));  
+  //   const newMovieDetailComponents = movies.results.map(movie => (
+  //     <MovieDetailComponent
+  //       key={movie.id}
+  //       title={movie.title}
+  //       overview={movie.overview}
+  //     />
+  //   ))
+  //   setMovieComponents(newMovieComponents);
+  //   setMovieDetailComponents(newMovieDetailComponents);
+  // }
+
+  // useState(() => {
+  //   generateMovieComponents();
+  // }, []);
+
+  const [movieData, setMovieData] = useState([]);
 
   useState(() => {
-    generateMovieComponents();
+    const newMovieData = movies.results.map(movie => ({
+      id: movie.id,
+      movieComponent: (
+        <MovieComponent
+          key={movie.id}
+          image={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+          title={movie.title}
+          voteAverage={movie.vote_average}
+        />
+      ),
+      movieDetailComponent: (
+        <MovieDetailComponent
+          key={movie.id}
+          title={movie.title}
+          overview={movie.overview}
+        />
+      )
+    }));
+    setMovieData(newMovieData);
   }, []);
 
   return (
     <div className='Container'>
       <div className='movieContainer'>
 
-        {MovieComponents.map((component, index) => (
+        {movieData.map(({ id, movieComponent, movieDetailComponent }) => (
+          <div key={id} className="movieItem">
+            {movieComponent}
+            <div className="movieItem">
+              {movieDetailComponent}
+            </div>
+          </div>
+        ))}   
+
+        {/* 두 개 따로 구현했을 때 -> 영화 리스트 다음에 설명 리스트가 붙어서 나옴 */}
+        {/* {MovieComponents.map((component, index) => (
           <div key={index} className="movieItem">
-          {component}
-        </div>
-      ))} 
-      <MovieDetailComponent/>
+            {component}
+          </div>
+        ))} 
+        
+        {MovieDetailComponents.map((detailComponent, index) => (
+          <div key={index} className="movieItem">
+            {detailComponent}
+          </div>
+        ))}  */}
       </div>
     </div>
   )
