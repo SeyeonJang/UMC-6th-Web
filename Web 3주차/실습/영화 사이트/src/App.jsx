ea5a53802c89cc32c4cb21a2328515cf04826c1f
SeyeonJang <seye0n.bliss@gmail.com>
2024-04-22 00:02:08 +0900

    feat: [#10] TMDB API로 실제 영화 데이터를 가져오는 로직 추가

--- Web 3주차/실습/영화 사이트/src/App.jsx
+++ Web 3주차/실습/영화 사이트/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { movies } from '../public/data/movies.jsx';
+import React, { useState, useEffect } from 'react';
+// import { movies } from '../public/data/movies.jsx';
 import MovieComponent from '../components/MovieComponent.jsx';
 import MovieDetailComponent from '../components/MovieDetailComponent.jsx';
 import './App.css';
@@ -7,27 +7,51 @@ import './App.css';
 function App() {
   const [movieData, setMovieData] = useState([]);
 
-  useState(() => {
-    const newMovieData = movies.results.map(movie => ({
-      id: movie.id,
-      movieComponent: (
-        <MovieComponent
-          key={movie.id}
-          image={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-          title={movie.title}
-          voteAverage={movie.vote_average}
-        />
-      ),
-      movieDetailComponent: (
-        <MovieDetailComponent
-          key={movie.id}
-          title={movie.title}
-          overview={movie.overview}
-        />
-      )
-    }));
-    setMovieData(newMovieData);
+  // fetch
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=ko-KRx&page=1', {
+          method: 'GET',
+          headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNzcwN2Q5ZWM3ZTY2OGE5OTk4NjUzYTdhNjMzMzU0NCIsInN1YiI6IjY2MjUxZDVjMjIxYmE2MDE2MzEzNDVhMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fpocj-TMRKkVquXQ4yX-KtpHI7h6CZJVHAvDNdd_PO4'
+          }
+        });
+        if (!response.ok) {
+          throw new Error('Error : Not OK :(');
+        }
+        const data = await response.json();
+        const movies = data.results;
+        console.log('Number of movies:', movies.length); // 영화 개수 출력
+        
+        const newMovieData = movies.map(movie => ({
+          id: movie.id,
+          movieComponent: (
+            <MovieComponent
+              key={movie.id}
+              image={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              title={movie.title}
+              voteAverage={movie.vote_average}
+            />
+          ),
+          movieDetailComponent: (
+            <MovieDetailComponent
+              key={movie.id}
+              title={movie.title}
+              overview={movie.overview}
+            />
+          )
+        }));
+        setMovieData(newMovieData);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+    fetchData();
   }, []);
+
+
 
   return (
     <div className='Container'>
