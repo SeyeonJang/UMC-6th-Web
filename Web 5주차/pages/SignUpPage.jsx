46307edc8fd4738db3a8a4d887bb00b2dd405e5d
SeyeonJang <seye0n.bliss@gmail.com>
2024-05-09 10:53:43 +0900

    feat: [#17] 이름, 이메일, 나이, 비밀번호, 비밀번호 확인 Validation 추가

--- Web 5주차/pages/SignUpPage.jsx
+++ Web 5주차/pages/SignUpPage.jsx
@@ -1,22 +1,64 @@
 import styled from 'styled-components';
-import { useState } from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
 
 function SignUpPage() {
-    const [isActive, setIsActive] = useState(true);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [age, setAge] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [isActive, setIsActive] = useState(false);
 
-    return(
+    const [nameError, setNameError] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [ageError, setAgeError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [confirmPasswordError, setConfirmPasswordError] = useState('');
+
+    useEffect(() => {
+        setNameError(!name ? '이름을 입력해주세요!' : '');
+        setEmailError(!email ? '이메일을 입력해주세요!' : !email.includes('@') ? '이메일 형식에 맞춰 다시 입력해주세요!' : '');
+        setAgeError(
+            !age ? '나이를 입력해주세요!' :
+            age < 0 ? '나이는 양수여야 합니다.' :
+            !Number.isInteger(parseFloat(age)) ? '나이는 정수여야 합니다.' :
+            age < 19 ? '본 영화 사이트는 19세 이상만 가입이 가능합니다.' : ''
+        );
+        setPasswordError(
+            !password ? '비밀번호를 입력해주세요!' :
+            password.length < 4 ? '비밀번호는 최소 4자리 이상 입력해주세요.' :
+            password.length > 12 ? '비밀번호는 최대 12자리 입니다.' :
+            !/[A-Za-z]/.test(password) || !/\d/.test(password) || !/[!@#$%^&*]/.test(password) ? '비밀번호는 영어, 숫자, 특수문자를 포함해주세요.' : ''
+        );
+        setConfirmPasswordError(
+            !confirmPassword ? '비밀번호를 다시 입력해주세요!' :
+            password !== confirmPassword ? '비밀번호가 일치하지 않습니다.' : ''
+        );
+
+        setIsActive(!nameError && !emailError && !ageError && !passwordError && !confirmPasswordError);
+    }, [name, email, age, password, confirmPassword, nameError, emailError, ageError, passwordError, confirmPasswordError]);
+
+
+    return (
         <Container>
             <SignupContainer>
                 <MainText>회원가입 페이지</MainText>
+
                 <FormWrapper>
-                    <InputBox type="text" id="name" placeholder="이름을 입력해주세요!"/>
-                    {/* <AlertText></AlertText> */}
-                    <InputBox type="text" id="email" placeholder="이메일을 입력해주세요!"/>
-                    <InputBox type="number" id="age" placeholder="나이를 입력해주세요!"/>
-                    <InputBox type="password" id="pw" placeholder="비밀번호를 입력해주세요!"/>
-                    <InputBox type="password" id="pw2" placeholder="비밀번호 확인"/>
-                    <SubmitButton>제출하기</SubmitButton>
+                    <InputBox type="text" placeholder="이름을 입력해주세요!" value={name} onChange={(e) => setName(e.target.value)}/>
+                    {nameError && <AlertText>{nameError}</AlertText>}
+                    <InputBox type="text" placeholder="이메일을 입력해주세요!" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    {emailError && <AlertText>{emailError}</AlertText>}
+                    <InputBox type="number" placeholder="나이를 입력해주세요!" value={age} onChange={(e) => setAge(e.target.value)}/>
+                    {ageError && <AlertText>{ageError}</AlertText>}
+                    <InputBox type="password" placeholder="비밀번호를 입력해주세요!" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    {passwordError && <AlertText>{passwordError}</AlertText>}
+                    <InputBox type="password" placeholder="비밀번호 확인" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
+                    {confirmPasswordError && <AlertText>{confirmPasswordError}</AlertText>}
+                    <SubmitButton isActive={isActive} disabled={!isActive} style={{backgroundColor: isActive ? 'orange' : 'white'}}>제출하기</SubmitButton>
                 </FormWrapper>
+                
                 <SubTextWrapper>
                     <SubText1>이미 아이디가 있으신가요?</SubText1>
                     <SubText2>로그인 페이지로 이동하기</SubText2>
@@ -24,6 +66,7 @@ function SignUpPage() {
             </SignupContainer>
         </Container>
     );
+    
 }
 
 const Container = styled.div`
@@ -85,8 +128,6 @@ const AlertText = styled.div`
 const SubmitButton = styled.button`
     width: 64%;
     height: 50px;
-    background-color: orange;
-    // background-color: ${({ isActive }) => (isActive ? 'orange' : 'white')};
     border-radius: 23px;
     font-size: 17px;
     margin-top: 40px;
