838183c86c7d7a102c9c5a75f36eaf45e8f01552
SeyeonJang <seye0n.bliss@gmail.com>
2024-05-26 18:09:00 +0900

    feat: [#26] 영화 페이지 Fetch를 Pagination으로 구현

--- Web 7주차/components/MoviesPaginationFetechComponent.jsx
+++ Web 7주차/components/MoviesPaginationFetechComponent.jsx
@@ -1,78 +1,108 @@
 import React, { useState, useEffect } from 'react';
+import ReactPaginate from "react-paginate";
 import styled from 'styled-components';
 
 import MovieComponent from './MovieComponent';
 import MovieDetailComponent from './MovieDetailComponent';
 import SpinnerComponent from './SpinnerComponent';
 
-function MoviesFetchComponent({address}) {
+function MoviesPaginationFetchComponent({address}) {
     const [movieData, setMovieData] = useState([]);
     const [spinner, setSpinner] = useState(false);
+    const [pageCount, setPageCount] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
 
-    // fetch
-    useEffect(() => {
-        const fetchData = async () => {
+    const fetchData = async (page) => {
         try {
             setSpinner(true);
-            const response = await fetch(address, {
-            method: 'GET',
-            headers: {
-                accept: 'application/json',
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNzcwN2Q5ZWM3ZTY2OGE5OTk4NjUzYTdhNjMzMzU0NCIsInN1YiI6IjY2MjUxZDVjMjIxYmE2MDE2MzEzNDVhMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fpocj-TMRKkVquXQ4yX-KtpHI7h6CZJVHAvDNdd_PO4'
-            }
+            const response = await fetch(`${address}&page=${page}`, {
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNzcwN2Q5ZWM3ZTY2OGE5OTk4NjUzYTdhNjMzMzU0NCIsInN1YiI6IjY2MjUxZDVjMjIxYmE2MDE2MzEzNDVhMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fpocj-TMRKkVquXQ4yX-KtpHI7h6CZJVHAvDNdd_PO4'
+                }
             });
             if (!response.ok) {
-            throw new Error('Error : Not OK :(');
+                throw new Error('Error : Not OK :(');
             }
             const data = await response.json();
             const movies = data.results;
-            console.log('Number of movies:', movies.length); // 영화 개수 출력
+            setPageCount(data.total_pages);
 
             const newMovieData = movies.map(movie => ({
-            id: movie.id,
-            movieComponent: (
-                <MovieComponent
-                key={movie.id}
-                id={movie.id}
-                image={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                title={movie.title}
-                voteAverage={movie.vote_average}
-                />
-            ),
-            movieDetailComponent: (
-                <MovieDetailComponent
-                key={movie.id}
-                id={movie.id}
-                title={movie.title}
-                overview={movie.overview}
-                originalTitle={movie.original_title}
-                />
-            )
+                id: movie.id,
+                movieComponent: (
+                    <MovieComponent
+                        key={movie.id}
+                        id={movie.id}
+                        image={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                        title={movie.title}
+                        voteAverage={movie.vote_average}
+                    />
+                ),
+                movieDetailComponent: (
+                    <MovieDetailComponent
+                        key={movie.id}
+                        id={movie.id}
+                        title={movie.title}
+                        overview={movie.overview}
+                        originalTitle={movie.original_title}
+                    />
+                )
             }));
             setMovieData(newMovieData);
             setSpinner(false);
         } catch (error) {
             console.error('Error:', error);
+            setSpinner(false);
         }
-        };
-        fetchData();
-    }, []);
+    };
+
+    // fetch
+    useEffect(() => {
+        fetchData(currentPage);
+    }, [currentPage]);
+
+    const handlePageClick = (event) => {
+        const selectedPage = event.selected + 1;
+        setCurrentPage(selectedPage);
+    };
 
     return(
         spinner 
             ? <SpinnerComponent/>
-            : <MovieContainer>
-            {movieData.map(({ id, movieComponent, movieDetailComponent }) => (
-                <MovieItem key={id} className="movieContainer__movieItem">
-                {movieComponent}
-                <MovieDetailWrapper className="movieContainer__movieItem__movieDetailWrapper">
-                    {movieDetailComponent}
-                </MovieDetailWrapper>
-                </MovieItem>
-            ))}
-        </MovieContainer>    
+            : <Wrapper>
+            <MovieContainer>
+                {movieData.map(({ id, movieComponent, movieDetailComponent }) => (
+                    <MovieItem key={id} className="movieContainer__movieItem">
+                    {movieComponent}
+                    <MovieDetailWrapper className="movieContainer__movieItem__movieDetailWrapper">
+                        {movieDetailComponent}
+                    </MovieDetailWrapper>
+                    </MovieItem>
+                ))}
+            </MovieContainer>
+            <ReactPaginate
+                breakLabel="..."
+                nextLabel=">"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={1}
+                pageCount={pageCount}
+                previousLabel="<"
+                renderOnZeroPageCount={null}
+                containerClassName={"pagination"}
+                activeClassName={"active"}
+            />    
+            </Wrapper>
     );
 }
+
+const Wrapper = styled.div`
+    width: 100%;
+    height: auto;
+    display: flex;
+    flex-direction: column;
+`;
 
 const MovieContainer = styled.div`
     width: 100%;
@@ -106,4 +136,4 @@ const MovieDetailWrapper = styled.div`
     display: none;
 `;
 
-export default MoviesFetchComponent;
+export default MoviesPaginationFetchComponent;
--- Web 7주차/pages/MoviePages/PopularPage.jsx
+++ Web 7주차/pages/MoviePages/PopularPage.jsx
@@ -1,10 +1,11 @@
-import MoviesFetchComponent from '../../components/MoviesFetchComponent';
+import MoviesPaginationFetchComponent from '../../components/MoviesPaginationFetechComponent';
 
 function PopularPage() {
-    const address = 'https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1';
+    // const address = 'https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1';
+    const address = 'https://api.themoviedb.org/3/movie/popular?language=ko-KR';
 
     return(
-        <MoviesFetchComponent address={address}/>
+        <MoviesPaginationFetchComponent address={address}/>
     );
 }
 
